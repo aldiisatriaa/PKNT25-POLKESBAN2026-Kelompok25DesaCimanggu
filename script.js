5f9ccd35783db60c1ef750610109accc8dbff94c
aldiisatriaa <aldisatria25k@gmail.com>
2026-01-15 02:33:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
-function hitungIMT() {
+function kirimData(event) {
+    event.preventDefault();
+
     const tinggi = document.getElementById("tinggi").value;
     const berat = document.getElementById("berat").value;
     const usia = document.getElementById("usia").value;
-    const hasil = document.getElementById("hasil");
-
-    if (tinggi === "" || berat === "" || usia === "") {
-        hasil.innerHTML = "Mohon lengkapi semua data.";
-        return;
-    }
 
     const tinggiMeter = tinggi / 100;
     const imt = berat / (tinggiMeter * tinggiMeter);
-    let kategori = "";
 
-    if (imt < 18.5) {
-        kategori = "Kurus";
-    } else if (imt < 25) {
-        kategori = "Normal";
-    } else if (imt < 30) {
-        kategori = "Kelebihan Berat Badan";
-    } else {
-        kategori = "Obesitas";
-    }
+    localStorage.setItem("dataIMT", JSON.stringify({
+        tinggi,
+        berat,
+        usia,
+        imt
+    }));
 
-    hasil.innerHTML = `
-        Nilai IMT Anda: ${imt.toFixed(2)} <br>
-        Kategori: ${kategori}
-    `;
+    window.location.href = "hasil.html";
 }
